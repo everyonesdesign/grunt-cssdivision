9361a026f3acb26954dbf3922e8dd0cc5dc9a5f6
Yura Trambitskiy <tr.yura@yahoo.com>
2014-11-03 22:49:53 +0300

    written some grunt plugin code

--- tasks/cssdivision.js
+++ tasks/cssdivision.js
@@ -8,43 +8,79 @@
 
 'use strict';
 
-module.exports = function(grunt) {
+function divideCSS(src, destDir, props) {
 
-  // Please see the Grunt documentation for more information regarding task
-  // creation: http://gruntjs.com/creating-tasks
+    var fs = require('fs'),
+        css = require('css'),
+        stylesheetText,
+        filename = src.replace(/(.*\/?)(\w\d\._\-)$/, "$2"),
+        msg = "";
 
-  grunt.registerMultiTask('cssdivision', 'Grunt plugin to divide CSS file into basic and decorative styles', function() {
-    // Merge task-specific and/or target-specific options with these defaults.
-    var options = this.options({
-      punctuation: '.',
-      separator: ', '
+    stylesheetText = fs.readFileSync(src, {
+        encoding: "UTF-8"
     });
 
-    // Iterate over all specified file groups.
-    this.files.forEach(function(f) {
-      // Concat specified files.
-      var src = f.src.filter(function(filepath) {
-        // Warn on and remove invalid source files (if nonull was set).
-        if (!grunt.file.exists(filepath)) {
-          grunt.log.warn('Source file "' + filepath + '" not found.');
-          return false;
-        } else {
-          return true;
+    function divideStylesheet(stylesheet, baseStyles) {
+        var parsedStylesheet = css.parse(stylesheet);
+        for (var i = 0; i < parsedStylesheet.stylesheet.rules.length; i++) {
+            if (parsedStylesheet.stylesheet.rules[i].declarations) {
+                for (var j = 0; j < parsedStylesheet.stylesheet.rules[i].declarations.length; j++) {
+                    var declaration = parsedStylesheet.stylesheet.rules[i].declarations[j],
+                        property = declaration.property;
+                    if (baseStyles && declaration.type != "declaration") {continue;}
+                    if (
+                        ( baseStyles && !props.test(property) ) ||
+                            ( !baseStyles && props.test(property) )
+                        ) {
+                        parsedStylesheet.stylesheet.rules[i].declarations.splice(j, 1);
+                        j--;
+                    }
+                }
+            }
         }
-      }).map(function(filepath) {
-        // Read file source.
-        return grunt.file.read(filepath);
-      }).join(grunt.util.normalizelf(options.separator));
+        return css.stringify(parsedStylesheet);
+    }
 
-      // Handle options.
-      src += options.punctuation;
+    try {
+        fs.writeFileSync(destDir.replace("\/?$", "/") + filename.replace(/\.\w+$/, ".main$0"), divideStylesheet(stylesheetText, true));
+        grunt.log.writeln('File "' + filename.replace(/\.\w+$/, ".main$0") + '" created.');
+        fs.writeFileSync(destDir.replace("\/?$", "/") + filename.replace(/\.\w+$/, ".decorative$0"), divideStylesheet(stylesheetText, false));
+        grunt.log.writeln('File "' + filename.replace(/\.\w+$/, ".decorative$0") + '" created.');
+    } catch(e) {
+        "Some error happened"
+    }
 
-      // Write the destination file.
-      grunt.file.write(f.dest, src);
+}
 
-      // Print a success message.
-      grunt.log.writeln('File "' + f.dest + '" created.');
+module.exports = function (grunt) {
+
+    // Please see the Grunt documentation for more information regarding task
+    // creation: http://gruntjs.com/creating-tasks
+
+    grunt.registerMultiTask('cssdivision', 'Grunt plugin to divide CSS file into basic and decorative styles', function () {
+        // Merge task-specific and/or target-specific options with these defaults.
+        var options = this.options({
+            props: /padding|margin|height|width|display|float|font/,
+            destDir: "dest/"
+        });
+
+        // Iterate over all specified file groups.
+        this.files.forEach(function (f) {
+            // Concat specified files.
+            var src = f.src.filter(function (filepath) {
+                // Warn on and remove invalid source files (if nonull was set).
+                if (!grunt.file.exists(filepath)) {
+                    grunt.log.warn('Source file "' + filepath + '" not found.');
+                    return false;
+                } else {
+                    return true;
+                }
+            }).map(function (filepath) {
+                // Read file source.
+                grunt.log.writeln('File "' + filepath + '" is being divided.');
+                divideCSS(filepath, options.destDir, options.props);
+            });
+        });
     });
-  });
 
 };
